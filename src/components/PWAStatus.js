e63d2d2cb112e91bb5d75693e67502b5d5d369df
Lijo Jose <lijojose43@gmail.com>
2025-10-17 03:29:02 +0530

    updates

--- src/components/PWAStatus.js
+++ src/components/PWAStatus.js
@@ -28,23 +28,7 @@ const PWAStatus = () => {
     };
   }, []);
 
-  return (
-    <div className="fixed top-4 left-4 z-40 flex gap-2">
-      {/* PWA Installation Status */}
-      {isInstalled && (
-        <div className="px-3 py-1 rounded-full text-xs font-medium bg-blue-100 text-blue-800 border border-blue-200 flex items-center gap-1">
-          <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          Installed
-        </div>
-      )}
-    </div>
-  );
+  return <div className="fixed top-4 left-4 z-40 flex gap-2"></div>;
 };
 
 export default PWAStatus;
